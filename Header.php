1d5cf3f80a254bd6c96f900781a68af7a7b8f7a0
Luke <hawadlu@outlook.com>
2019-07-01 17:55:04 +1200

    Removes nav and improves Header

--- Header.php
+++ Header.php
@@ -2,11 +2,15 @@
 <link rel="stylesheet" href="styles.css">
 <!--Makes the page scale properly for mobile devices-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<div style="background-color: #0c59cf;color: white;margin: 0px 0px auto 0px;">
+<div class="header">
         <!--This is what is shown at the top of the page-->
 			<center>
 					<!--Shows the companmy name-->
-					<h1>Luke Hawinkels</h1>
+					<h1>
+						<strong>
+							Luke Hawinkels
+						</strong>
+					</h1>
 			</center>
                 
         </div>
--- Nav.php
+++ Nav.php
@@ -3,11 +3,4 @@
 <!--Connects to the styleseet-->
 <link rel="stylesheet" href="styles.css">
 <!--The divs that contain each of the nav options-->
-<center>
-</center><div style="background-color: #123456;" class="grid-container-nav">
-      <center><div><a class=nav href="Index.php">Home</a><hr></div></center>
-      <center><div><a class=nav href="Education_Experience.php">Education and Experience</a><hr></div></center>
-      <center><div><a class=nav href="Examples.php">Examples</a><hr></div></center>
-      <center><div><a class=nav href="Contact.php">Contact</a><hr></div></center>
-</div>
-</center>
+
