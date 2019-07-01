941efd3cd3fc34811cb30267d3852ae94a1ecdef
Luke <hawadlu@outlook.com>
2019-07-01 17:40:32 +1200

    Updates header colour

--- Header.php
+++ Header.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="styles.css">
 <!--Makes the page scale properly for mobile devices-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<div style="background-color: #EB621A;color: white;margin: 0px 0px auto 0px;">
+<div style="background-color: #0c59cf;color: white;margin: 0px 0px auto 0px;">
         <!--This is what is shown at the top of the page-->
 			<center>
 					<!--Shows the companmy name-->
