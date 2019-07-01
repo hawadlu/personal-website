f68f8f4e0b108f5b0bf53cfb26fdad09b9150163
Luke <hawadlu@outlook.com>
2019-07-01 19:22:12 +1200

    Adds grid layout for entire page

--- Index.php
+++ Index.php
@@ -1,26 +1,35 @@
 
 <html>
 <!--Pulls in the head and other required pages-->
-<?php require("Head.php") ?>       
-<body>
-    <header>
-        
-        <div id="header-nav-container">
+<?php require("Head.php") ?>
+<div class="page-grid-container">       
+    <!--The first div of the page grid-->
+    <div class="page-grid-container">  
+        <div style="background-color: red">
             <?php
             require("Header.php");
             require("Nav.php");
             ?>
         </div>
-        
-    </header>
+    </div>
+
+    <!--The second div of the page grid-->
+    <div>
+        <!--The grid which contains the main content of the page-->
+        <div class="grid-container">
+            <div class="Title"><p>One</p></div>
+            <div class="Image"><p>One</p></div>
+            <div class="Description"><p>One</p></div>
+            <div class="Other"><p>One</p></div>
+        </div>  
+    </div>
     
-    <body>      
-    </body>
-    <footer>
-        <!-- Footer -->
-        <?php
-            //Pull information from the footer page
+    
+    
+</div>
+<!-- Footer -->
+<?php
+        //Pull information from the footer page
             require("Footer.php");//'Require is 100% needed for this site to run
             ?>
-        </footer>
-        </html>
+            </html>
