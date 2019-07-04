d5c1e51670b55f00c7c3279c55712b0d89db023d
Luke <hawadlu@outlook.com>
2019-07-05 10:53:46 +1200

    Update educationAndExperience.php
    Query now outputs all results to the screen. Needs to be tidied up.

--- educationAndExperience.php
+++ educationAndExperience.php
@@ -29,41 +29,6 @@ require("connect.php")?>
 		</div>
 
 		<div id="Education" class="tabcontent">
-			<div class="education-grid-container">
-				<div class="education-Institution">
-					<center>
-						<h1>
-							Institution
-						</h1>
-					</center>
-				</div>
-				<div class="education-Subject">
-					<center>
-						<p><strong>Subject: (abbreviation here)</strong>xyz</p>
-					</center>
-				</div>
-				<div class="education-Grade">
-					<center>
-						<p>
-							Grade: E
-						</p>
-					</center>
-				</div>
-				<div class="education-Credits">
-					<center>
-						<p>
-							Credits: 25
-						</p>
-					</center>
-				</div>
-				<div class="education-Year">
-					<center>
-						<p>
-							Year: 2019
-						</p>
-					</center>
-				</div>
-			</div>
 			<?php
         	//The query which shows the education history
 			$EducationQuery = ("SELECT `Education`.`subjectFK`, `Subject`.`subject`, `Education`.`institutionFK`, `Institution`.`institution`, `Education`.`gradeFk`, `Grade`.`grade`, `Education`.`subjectLevelFK`, `subjectLevel`.`subjectLevel`, `Education`.`credits`, `Education`.`classYearFK`, `relevantYear`.`relevantYear`, `Education`.`subjectAbbreviationFK`, `subjectAbbreviation`.`subjectAbbreviation`, `Education`.`endorsementFK`, `Endorsement`.`endorsement`
@@ -75,72 +40,129 @@ require("connect.php")?>
 				LEFT JOIN `relevantYear` ON `Education`.`classYearFK` = `relevantYear`.`relevantYearPK` 
 				LEFT JOIN `subjectAbbreviation` ON `Education`.`subjectAbbreviationFK` = `subjectAbbreviation`.`subjectAbbreviationPK` 
 				LEFT JOIN `Endorsement` ON `Education`.`endorsementFK` = `Endorsement`.`endorsementPK`
-				ORDER BY `Education`.`institutionFK` ASC;
+				ORDER BY `relevantYear`.`relevantYear` DESC;
 				");
 
-				$EducationResult = mysqli_query($con,$EducationQuery);
-				while($EducationOutput=mysqli_fetch_array($EducationResult)) {
-					echo 'hello';
-				}
+			$EducationResult = mysqli_query($con,$EducationQuery);
+			while($EducationOutput=mysqli_fetch_array($EducationResult)) {
 				?>
-
-
-			</div>
-
-			<div id="Experience" class="tabcontent">
-				<div class="experience-examples-grid-container">
-					<div class="experience-examples-name">
+				
+				<div class="education-grid-container">
+					<div class="education-Institution">
 						<center>
 							<h1>
-								Experience Name
+								<?php
+								echo $EducationOutput['institution'];
+								?>
 							</h1>
 						</center>
 					</div>
-					<div class="experience-examples-year">
+					<div class="education-Subject">
 						<center>
 							<p>
-								Year: 2019
+								<strong>
+									Subject: 
+									<?php
+										echo '(' . $EducationOutput['subjectAbbreviation'] . ')';
+									?>
+								</strong>
+								<?php
+									echo $EducationOutput['subject'];
+								?>
 							</p>
 						</center>
 					</div>
-					<div class="experience-examples-langauges">
+					<div class="education-Grade">
 						<center>
 							<p>
-								language(s): HTML, CSS, JavaScript
+								Grade: 
+								<?php
+									echo $EducationOutput['grade'];
+								?>
 							</p>
 						</center>
 					</div>
-					<div class="experience-examples-link">
+					<div class="education-Credits">
 						<center>
 							<p>
-								link: abcdef
+								Credits: 
+								<?php
+									echo $EducationOutput['credits'];
+								?>
 							</p>
 						</center>
 					</div>
-					<div class="experience-examples-description">
+					<div class="education-Year">
 						<center>
 							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac elementum tellus. Pellentesque vehicula metus eu posuere vulputate. Aliquam erat volutpat. Sed metus nibh, malesuada consectetur dolor a, commodo convallis sapien. Vestibulum ex magna, laoreet vitae nibh feugiat, sodales vestibulum eros. Nullam gravida ultricies magna, quis fringilla est posuere nec. Integer congue cursus eros, quis maximus orci. In hac habitasse platea dictumst. Nam tincidunt, nisl eget eleifend posuere, nisi ex gravida ex, vitae blandit felis erat a urna. Sed id quam odio. Sed suscipit id nunc ut porttitor. Suspendisse feugiat eget erat sit amet dapibus. Donec auctor velit vitae quam lobortis dignissim.
+								Year: 
+								<?php
+									echo $EducationOutput['relevantYear'];
+								?>
 							</p>
 						</center>
 					</div>
 				</div>
-			</div>
+				<?php
+			}
+			?>
 
-			<script>
-				function showEducationAndExperience(evt, cityName) {
-					var i, tabcontent, tablinks;
-					tabcontent = document.getElementsByClassName("tabcontent");
-					for (i = 0; i < tabcontent.length; i++) {
-						tabcontent[i].style.display = "none";
-					}
-					tablinks = document.getElementsByClassName("tablinks");
-					for (i = 0; i < tablinks.length; i++) {
-						tablinks[i].className = tablinks[i].className.replace(" active", "");
-					}
-					document.getElementById(cityName).style.display = "block";
-					evt.currentTarget.className += " active";
+		</div>
+
+		<div id="Experience" class="tabcontent">
+			<div class="experience-examples-grid-container">
+				<div class="experience-examples-name">
+					<center>
+						<h1>
+							Experience Name
+						</h1>
+					</center>
+				</div>
+				<div class="experience-examples-year">
+					<center>
+						<p>
+							Year: 2019
+						</p>
+					</center>
+				</div>
+				<div class="experience-examples-langauges">
+					<center>
+						<p>
+							language(s): HTML, CSS, JavaScript
+						</p>
+					</center>
+				</div>
+				<div class="experience-examples-link">
+					<center>
+						<p>
+							link: abcdef
+						</p>
+					</center>
+				</div>
+				<div class="experience-examples-description">
+					<center>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac elementum tellus. Pellentesque vehicula metus eu posuere vulputate. Aliquam erat volutpat. Sed metus nibh, malesuada consectetur dolor a, commodo convallis sapien. Vestibulum ex magna, laoreet vitae nibh feugiat, sodales vestibulum eros. Nullam gravida ultricies magna, quis fringilla est posuere nec. Integer congue cursus eros, quis maximus orci. In hac habitasse platea dictumst. Nam tincidunt, nisl eget eleifend posuere, nisi ex gravida ex, vitae blandit felis erat a urna. Sed id quam odio. Sed suscipit id nunc ut porttitor. Suspendisse feugiat eget erat sit amet dapibus. Donec auctor velit vitae quam lobortis dignissim.
+						</p>
+					</center>
+				</div>
+			</div>
+		</div>
+
+		<script>
+			function showEducationAndExperience(evt, cityName) {
+				var i, tabcontent, tablinks;
+				tabcontent = document.getElementsByClassName("tabcontent");
+				for (i = 0; i < tabcontent.length; i++) {
+					tabcontent[i].style.display = "none";
 				}
+				tablinks = document.getElementsByClassName("tablinks");
+				for (i = 0; i < tablinks.length; i++) {
+					tablinks[i].className = tablinks[i].className.replace(" active", "");
+				}
+				document.getElementById(cityName).style.display = "block";
+				evt.currentTarget.className += " active";
+			}
     // Get the element with id="defaultOpen" and click on it
     document.getElementById("defaultOpen").click();
 </script>
