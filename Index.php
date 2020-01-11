c33c3d2db7dd543ec7e227ecba5e2c10f1940f5d
Luke <hawadlu@outlook.com>
2020-01-11 14:53:16 +1300

    Adds background slideshow to home screen.

--- Index.php
+++ Index.php
@@ -1,52 +1,46 @@
-
 <html>
-<!--Pulls in the head and other required pages-->
-<?php require("head.php") ?>  
-<div class="page-grid-container">     
-    <!--The first div of the page grid-->
-    <div>
+    <head>
         <?php
-        require("header.php");
-        require("nav.php");
+        require("head.php");
+        require("Header.php");
+        require("Nav.php");
         ?>
-    </div>
+    </head>
 
-    <!--The second div of the page grid-->
-    <div>
-        <!--The grid which contains the main content of the page-->
-        <div class="aboutMe-grid-container">
-            <div class="aboutMe-Title">
-                <center>
-                    <h1>
-                        About Me
-                    </h1>
-                </center>
-            </div>
-            <div class="aboutMe-Image">
-                <div style="text-align: center;">
-                    <img src="Images/Profile Pic.png" alt="Me">
-                </div>
-            </div>
-            <div class="aboutMe-Description">
-                <p>
-                    I have chosen to go down the software development route because I feel that the teamwork environment that the industry provides is a natural fit for my personality. I enjoy working as part of a team (especially sports and development teams). I find coding a program with constraints to be an excellent learning opportunity and an enjoyable challenge.
-                    <br><br>
-                    I have had five years of experience coding at a college level. This has included learning languages such as Python, PHP, SQL, CSS and HTML.
-                    <br><br>
-                    I am currently at University and practicing the languages C++, Python and Java.
-                    <br><br>
-                    I can bring an element of leadership and teamwork as well a hard working, motivated personality to any role. While in the New Zealand Cadet Corp I was a Flight Seargent which meant that I had to deliver 45 minute lessons to teenagers on a consistent basis. This involved planning the lessons while working around the tasks that I have from University and a part time job as a waiter. This has taught me valuable leadership, communication and time management skills which I think would be a great fit for any company.
-                </p>
-            </div>
-        </div>  
-    </div>
-
-
-
-</div>
-<!-- Footer -->
-<?php
-        //Pull information from the footer page
-            require("footer.php");//'Require is 100% needed for this site to run
+    <!--Disable scrolling-->
+    <body style = "margin: 0; height: 100%; overflow: hidden;">
+            <?php
+            //Load BG Images
+            foreach (glob("BG Images/*") as $file) {
             ?>
-            </html>
+            <div class="bgImage fade">
+                        <img src="<?php echo $file; ?>" class="center" style = "border-radius: 0px; width: 100%">
+            </div>
+                <?php
+            }
+            ?>
+
+
+        <script>
+            var slideIndex = 0;
+            showSlides();
+
+            function showSlides() {
+                var i;
+                var slides = document.getElementsByClassName("bgImage");
+                for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none";
+                }
+                slideIndex++;
+                if (slideIndex > slides.length) {slideIndex = 1}
+                slides[slideIndex-1].style.display = "block";
+                setTimeout(showSlides, 5000); // Change image every 2 seconds
+            }
+        </script>
+    </body>
+
+
+    <?php
+    require("Footer.php");
+    ?>
+</html>
