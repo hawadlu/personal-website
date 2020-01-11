0dd1d8ffcde178d937f975a2334028e9be628fa8
Luke <hawadlu@outlook.com>
2020-01-11 14:51:42 +1300

    Moves personal information to About.php

---
+++ About.php
@@ -0,0 +1,52 @@
+
+<html>
+<!--Pulls in the head and other required pages-->
+<?php require("head.php") ?>  
+<div class="page-grid-container">     
+    <!--The first div of the page grid-->
+    <div>
+        <?php
+        require("Header.php");
+        require("Nav.php");
+        ?>
+    </div>
+
+    <!--The second div of the page grid-->
+    <div>
+        <!--The grid which contains the main content of the page-->
+        <div class="aboutMe-grid-container">
+            <div class="aboutMe-Title">
+                <center>
+                    <h1>
+                        About Me
+                    </h1>
+                </center>
+            </div>
+            <div class="aboutMe-Image">
+                <div style="text-align: center;">
+                    <img src="Images/Profile Pic.png" alt="Me">
+                </div>
+            </div>
+            <div class="aboutMe-Description">
+                <p>
+                    I have chosen to go down the software development route because I feel that the teamwork environment that the industry provides is a natural fit for my personality. I enjoy working as part of a team (especially sports and development teams). I find coding a program with constraints to be an excellent learning opportunity and an enjoyable challenge.
+                    <br><br>
+                    I have had five years of experience coding at a college level. This has included learning languages such as Python, PHP, SQL, CSS and HTML.
+                    <br><br>
+                    I am currently at University and practicing the languages C++, Python and Java.
+                    <br><br>
+                    I can bring an element of leadership and teamwork as well a hard working, motivated personality to any role. While in the New Zealand Cadet Corp I was a Flight Seargent which meant that I had to deliver 45 minute lessons to teenagers on a consistent basis. This involved planning the lessons while working around the tasks that I have from University and a part time job as a waiter. This has taught me valuable leadership, communication and time management skills which I think would be a great fit for any company.
+                </p>
+            </div>
+        </div>  
+    </div>
+
+
+
+</div>
+<!-- Footer -->
+<?php
+        //Pull information from the footer page
+            require("footer.php");//'Require is 100% needed for this site to run
+            ?>
+            </html>
--- Nav.php
+++ Nav.php
@@ -1,38 +1,47 @@
 <!--Allows the nav to scale for mobile devices-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!--Connects to the styleseet-->
-<link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="Styles.css">
 <!--The divs that contain each of the nav options-->
 <div class="nav-grid-container">
-	
+    <div class="hover-effect">
+        <div style="text-align: center;">
+            <a href="Index.php">
+                <p>
+                    Home
+                </p>
+            </a>
+        </div>
+
+    </div>
 	<div class="hover-effect">
-		<center>
-			<a href="index.php">
+		<div style="text-align: center;">
+			<a href="About.php">
 				<p>
-					Home
+					About Me
 				</p>
 			</a>
-		</center>
+		</div>
 		
 	</div>
 	<div class="hover-effect">
-		<center>
-			<a href="examples.php">
+		<div style="text-align: center;">
+			<a href="Examples.php">
 				<p>
 					Examples
 				</p>
 			</a>
-		</center>
+		</div>
 		
 	</div>
 	<div class="hover-effect">
-		<center>
-			<a href="education.php">
+		<div style="text-align: center;">
+			<a href="Education.php">
 				<p>
 					Education
 				</p>
 			</a>
-		</center>
+		</div>
 		
 	</div>
 
