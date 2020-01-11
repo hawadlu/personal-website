13eab8034e12e06d1772f8b9ec16cd0491d3dc3d
Luke <hawadlu@outlook.com>
2020-01-11 13:44:49 +1300

    Updates index page

--- Index.php
+++ Index.php
@@ -33,7 +33,7 @@
                     <br><br>
                     I have had five years of experience coding at a college level. This has included learning languages such as Python, PHP, SQL, CSS and HTML.
                     <br><br>
-                    I am currently at University and practicing the languages C++ and Java.
+                    I am currently at University and practicing the languages C++, Python and Java.
                     <br><br>
                     I can bring an element of leadership and teamwork as well a hard working, motivated personality to any role. While in the New Zealand Cadet Corp I was a Flight Seargent which meant that I had to deliver 45 minute lessons to teenagers on a consistent basis. This involved planning the lessons while working around the tasks that I have from University and a part time job as a waiter. This has taught me valuable leadership, communication and time management skills which I think would be a great fit for any company.
                 </p>
