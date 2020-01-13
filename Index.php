fae9d6553642a96d55c93092f0dedb02dedabe3e
Luke <hawadlu@outlook.com>
2020-01-14 11:34:18 +1300

    Remove current background slideshow from index page

--- Index.php
+++ Index.php
@@ -9,38 +9,7 @@
 
     <!--Disable scrolling-->
     <body style = "margin: 0; height: 100%; overflow: hidden;">
-    <!--Navigation buttons-->
-    <div class="home-buttons-container">
-        <a href = "About.php">About Me</a>
-    </div>
-            <?php
-            //Load BG Images
-            foreach (glob("BG Images/*") as $file) {
-            ?>
-            <div class="bgImage fade">
-                        <img src="<?php echo $file; ?>" class="center" style = "border-radius: 0px; width: 100%">
-            </div>
-                <?php
-            }
-            ?>
-
-
-        <script>
-            var slideIndex = 0;
-            showSlides();
-
-            function showSlides() {
-                var i;
-                var slides = document.getElementsByClassName("bgImage");
-                for (i = 0; i < slides.length; i++) {
-                    slides[i].style.display = "none";
-                }
-                slideIndex++;
-                if (slideIndex > slides.length) {slideIndex = 1}
-                slides[slideIndex-1].style.display = "block";
-                setTimeout(showSlides, 5000); // Change image every 2 seconds
-            }
-        </script>
+    
     </body>
 
 
