f50b7ee9cec9de3d30c1894f8b4ea0c2177a204a
Luke <hawadlu@outlook.com>
2020-01-28 12:06:47 +1300

    Adds ability to upload multiple files simultaneously.

--- edit.php
+++ edit.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['loggedin'])) {
     exit();
 }
 
-echo "It's lonely here";
+
 ?>
 <html lang="English">
     <!--Pulls in the head and other required pages-->
@@ -32,12 +32,96 @@ echo "It's lonely here";
                 <div class="newItemModalContent">
                     <span class="closeNewItemModal">&times;</span>
                     <!--Todo for play around file uploads it is probably best not to upload the file, but to store the file path instead and use that to display the image-->
-                    <!--Upload files-->
-                    <form action="createItem.php" method="post" enctype="multipart/form-data">
+                    <!--Upload files. Allow up to five-->
+                    <form action="" method="post" enctype="multipart/form-data">
                         Select image to upload:
-                        <input type="file" name="fileToUpload" id="fileToUpload">
-                        <input type="submit" value="Upload Image" name="submit">
+                        <input type="file" name="userFiles[]" id="" multiple="">
+                        <input type="submit" value="Upload" name="submit">
                     </form>
+
+                    <!--Uploading the files-->
+                    <?php
+                    //Check the upload files form has been submitted
+                    if (isset($_FILES['userFiles'])) {
+                        //useful functions and variables. Credit to "Clever Techie. https://www.youtube.com/watch?v=KXyMpRp4d2Q"
+                        //Array of possible file upload errors
+                        $phpFileUploadErrors = array(
+                            0 => "The file uploaded successfully",
+                            1 => "The file exceeds the maximum file size defined in php.ini",
+                            2 => "The file exceeds the maximum file size defines in the HTML form",
+                            3 => "The uploaded file was only partially uploaded",
+                            4 => "No file was uploaded",
+                            6 => "Missing a temporary folder",
+                            7 => "Filed to write file to the disc",
+                            8 => "A php extension stopped the file from uploading"
+                        );
+
+                        $file_array = reArrayFiles($_FILES['userFiles']);
+                        pre_r($file_array);
+
+                        for ($i = 0; $i < count($file_array); $i++) {
+                            //Check for errors
+                            if ($file_array[$i]['error']) {
+                                ?>
+                                <div class="alert alert-danger">
+                                    <?php echo $file_array[$i]['name'] . " " . $phpFileUploadErrors[$file_array[$i]['error']]; ?>
+                                </div>
+                                <?php
+
+                                //Check for extensions errors
+                            } else {
+                                //Allowable file types
+                                $extensions = array("jpg", "png", "gif", "jpeg");
+                                $file_ext = explode(".", $file_array[$i]["name"]);
+                                $file_ext = end($file_ext);
+
+                                //Check if the extension is acceptable
+                                if (!in_array($file_ext, $extensions)) {
+                                    ?>
+                                    <div class="alert alert-danger">
+                                        <?php echo $file_array[$i]["name"] . " Invalid file extension!"; ?>
+                                    </div>
+                                    <?php
+                                } else {
+                                    //File uploaded successfully
+                                    //Move the file from the temporary directory to the intended directory
+                                    move_uploaded_file($file_array[$i]["tmp_name"], "images/" . $file_array[$i]["name"]);
+
+                                    //Print a success message
+                                    ?>
+                                    <div class="alert alert-success">
+                                        <?php echo $file_array[$i]["name"] . " " . $phpFileUploadErrors[$file_array[$i]["error"]]?>
+                                    </div>
+                                    <?php
+                                }
+                            }
+                        }
+                    }
+
+                    //Converts $_FILES to a cleaner array when uploading multiple files
+                    function reArrayFiles($file_post) {
+                        $file_ary = array();
+                        $file_count = count($file_post['name']);
+                        $file_keys = array_keys($file_post);
+
+                        for ($i = 0; $i < $file_count; $i++) {
+                            foreach ($file_keys as $key) {
+                                $file_ary[$i][$key] = $file_post[$key][$i];
+                            }
+                        }
+
+                        return$file_ary;
+                    }
+
+                    //Same as print_r surrounded with <pre></pre> HTML tags for better array readability
+                    function pre_r($array) {
+                        echo '<pre>';
+                        print_r($array);
+                        echo '</pre>';
+                    }
+
+
+                    ?>
                 </div>
 
             </div>
@@ -76,10 +160,8 @@ echo "It's lonely here";
     </script>
     <!--Called last so that it renders at the top-->
     <?php
-        require("header.php");;
+        //require("header.php");;
         //Pull information from the footer page
         require("footer.php");
     ?>
 </html>
-
-?>
